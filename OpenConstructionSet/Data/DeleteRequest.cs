66466f30764ec5ab8423bcb49709f1bb1668c7ac
Luke May <12233765+lmaydev@users.noreply.github.com>
2025-02-05 23:03:56 +0000

    Separated files

--- OpenConstructionSet/Data/DeleteRequest.cs
+++ OpenConstructionSet/Data/DeleteRequest.cs
@@ -1,18 +1,3 @@
 ﻿namespace OpenConstructionSet.Data;
 
 public readonly record struct DeleteRequest(uint Version, string Items);
-
-public record struct ItemSaveData(uint SaveCount, ItemChangeType ChangeType)
-{
-    public ItemSaveData(uint value) : this(value >> 4, (ItemChangeType)(value & 0xF))
-    {
-
-    }
-
-    public static implicit operator uint(ItemSaveData data)
-        => (uint)data.ChangeType | (data.SaveCount << 4);
-
-    public static implicit operator ItemSaveData(uint value) => new(value);
-}
-
-public readonly record struct MergeEntry(uint Version1, uint Version2);
