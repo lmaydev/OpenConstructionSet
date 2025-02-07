97a0db840f2dbb71c9ce00c1fcc34ec3d4cbe6ab
Luke May <12233765+lmaydev@users.noreply.github.com>
2025-02-07 13:46:27 +0000

    Add setting and save folder helpers. Updated DI

---
+++ OpenConstructionSet/Installations/Settings/SettingException.cs
@@ -0,0 +1,19 @@
+﻿namespace OpenConstructionSet.Installations.Settings;
+
+public class SettingException : Exception
+{
+    public SettingException()
+    {
+    }
+
+    public SettingException(string? message) : base(message)
+    {
+    }
+
+    public SettingException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    public static SettingException ReadFailed(string settingName, Exception? innerException)
+        => new SettingException($"Failed to read setting {settingName}", innerException);
+}
